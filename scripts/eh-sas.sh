4a40e12a9ac00db6151514068535c72ceecbadfb
timwebster9 <tim.webster@gmail.com>
2021-07-28 15:42:56 +0100

    update policy

--- scripts/eh-sas.sh
+++ scripts/eh-sas.sh
@@ -3,7 +3,7 @@
 get_sas_token() {
     local EVENTHUB_URI="timwehn345454345.servicebus.windows.net/timwehn345454345"
     local SHARED_ACCESS_KEY_NAME="diagnostics"
-    local SHARED_ACCESS_KEY="Y8xaXVXfFU1gSiQMq4Y/NRVmEIzkVESSckhRYfyXVZM="
+    local SHARED_ACCESS_KEY="PRIMARY OR SECONDARY KEY"
     local EXPIRY=${EXPIRY:=$((60 * 60 * 24))} # Default token expiry is 1 day
 
     local ENCODED_URI=$(echo -n $EVENTHUB_URI | jq -s -R -r @uri)
@@ -20,9 +20,3 @@ get_sas_token() {
 }
 
 get_sas_token
-
-# EH SAS:
-# https://timwehn345454345.servicebus.windows.net/timwehn345454345?sr=timwehn345454345.servicebus.windows.net%2Ftimwehn345454345&sig=Vam3GlnVkOTpOvKZXpdb%2FjqWInVqB8%2FQM64AVdDpvdU%3D&se=1627473160&skn=diagnostics
-
-# storage SAS:
-# sv=2020-08-04&ss=bt&srt=co&sp=wlacutfx&se=2021-08-06T22:08:39Z&st=2021-07-26T14:08:39Z&spr=https&sig=ILUR757rpJTdv55OTVSNzJQW9rIM4ARz8O0dfdcISHM%3D
